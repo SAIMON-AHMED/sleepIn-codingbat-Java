ad33e2c3f593fedb98220f74d502b11388125725
Saimon Ahmed <67533318+SAIMON-AHMED@users.noreply.github.com>
2021-09-22 15:34:56 +0600

    Created sleepIn.java

---
+++ sleepIn.java
@@ -0,0 +1,4 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  return (weekday == false || vacation == true);//when weekday false or vacation true,
+                                                // we will return true.
+}
